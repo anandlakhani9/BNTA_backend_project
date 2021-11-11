54f39a6c3c76a6c36ada741d0295fc9b6613cecc
Rose <roset@roses-air.home>
2021-11-11 09:17:23 +0000

    Spring initialiser setup

---
+++ dndspellapi/src/main/java/com/drumandbase/dndspellapi/DndspellapiApplication.java
@@ -0,0 +1,13 @@
+package com.drumandbase.dndspellapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DndspellapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DndspellapiApplication.class, args);
+	}
+
+}
---
+++ dndspellapi/src/test/java/com/drumandbase/dndspellapi/DndspellapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drumandbase.dndspellapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DndspellapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
