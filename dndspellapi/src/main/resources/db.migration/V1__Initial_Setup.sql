d2f6d08506e987b36fbe515b14ee5226352dd114
Rose <roset@roses-air.home>
2021-11-11 12:42:20 +0000

    completed initial setup for sql tables

---
+++ dndspellapi/src/main/resources/db.migration/V1__Initial_Setup.sql
@@ -0,0 +1,50 @@
+CREATE TABLE schools (id SERIAL PRIMARY KEY, school_name TEXT NOT NULL, school_description TEXT NOT NULL);
+
+CREATE TABLE spells (id BIGSERIAL PRIMARY KEY,
+    spell_name TEXT NOT NULL,
+    spell_level INT NOT NULL,
+    school_id INT NOT NULL REFERENCES schools(id),
+    range TEXT,
+    components TEXT,
+    duration TEXT,
+    ritual BOOLEAN,
+    descr TEXT,
+    higher_level TEXT
+    );
+
+CREATE TABLE dndclasses (id SERIAL PRIMARY KEY NOT NULL, class_name TEXT);
+CREATE TABLE dndraces (id SERIAL PRIMARY KEY NOT NULL, race_name TEXT);
+
+CREATE TABLE characters (id BIGSERIAL NOT NULL PRIMARY KEY,
+    class_id INT NOT NULL REFERENCES dndclasses(id),
+    race_id INT NOT NULL REFERENCES dndraces(id),
+    character_level INT NOT NULL,
+    name TEXT NOT NULL,
+    max_cantrips_known INT NOT NULL,
+    max_spells_known int NOT NULL,
+    spell_slot_1 INT NOT NULL,
+    spell_slot_2 INT NOT NULL,
+    spell_slot_3 INT NOT NULL,
+    spell_slot_4 INT NOT NULL,
+    spell_slot_5 INT NOT NULL,
+    spell_slot_6 INT NOT NULL,
+    spell_slot_7 INT NOT NULL,
+    spell_slot_8 INT NOT NULL,
+    spell_slot_9 INT NOT NULL,
+    max_spell_slot_1 INT NOT NULL,
+    max_spell_slot_2 INT NOT NULL,
+    max_spell_slot_3 INT NOT NULL,
+    max_spell_slot_4 INT NOT NULL,
+    max_spell_slot_5 INT NOT NULL,
+    max_spell_slot_6 INT NOT NULL,
+    max_spell_slot_7 INT NOT NULL,
+    max_spell_slot_8 INT NOT NULL,
+    max_spell_slot_9 INT NOT NULL,
+    );
+
+CREATE TABLE character_spells (id BIGSERIAL NOT NULL,
+    character_id BIGINT NOT NULL REFERENCES characters(id),
+    spell_id BIGINT NOT NULL REFERENCES spells(id)
+    );
+
+
