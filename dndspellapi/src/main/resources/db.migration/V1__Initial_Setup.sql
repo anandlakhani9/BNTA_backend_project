d35fea5444e1f77b1b166fef5012914283ac4ef5
Rose <roset@roses-air.home>
2021-11-15 14:57:34 +0000

    added schools of magic in sql inserts

--- dndspellapi/src/main/resources/db.migration/V1__Initial_Setup.sql
+++ dndspellapi/src/main/resources/db.migration/V1__Initial_Setup.sql
@@ -9,7 +9,15 @@ CREATE TABLE spells (id BIGSERIAL PRIMARY KEY,
     duration TEXT,
     ritual BOOLEAN,
     description TEXT,
-    higher_level TEXT
+    higher_level TEXT,
+    canSorcerer BOOLEAN,
+    canWizard BOOLEAN,
+    canWarlock BOOLEAN,
+    canBard BOOLEAN,
+    canPaladin BOOLEAN,
+    canDruid BOOLEAN,
+    canCleric BOOLEAN,
+    canRanger BOOLEAN
     );
 
 CREATE TABLE dndclasses (id SERIAL PRIMARY KEY, class_name TEXT, class_description TEXT);
@@ -47,4 +55,17 @@ CREATE TABLE character_spells (id BIGSERIAL PRIMARY KEY,
     spell_id BIGINT NOT NULL REFERENCES spells(id)
     );
 
-
+INSERT INTO schools (school_name, school_description)
+VALUES ('Abjuration','Spells are protective in  nature, though some of them  have aggressive uses. They create magical barriers, negate harmful effects,  harm trespassers, or banish creatures to other planes of existence.');
+INSERT INTO schools (school_name, school_description)
+VALUES ('Conjuration','Spells involve the transportation of objects and creatures from one location to another. Some spells summon creatures or objects to the caster’s side, whereas others allow the caster to teleport to another location. Some conjurations create objects or effects out of nothing. Divination spells reveal information, whether in the form of secrets long forgotten, glimpses of the future, the locations of hidden things, the truth behind illusions, or visions of distant people or places.');
+INSERT INTO schools (school_name, school_description)
+VALUES ('Enchantment','Spells affect the minds of others, influencing or controlling their behavior. Such spells can  make enemies see the caster as a friend, force creatures to take a course of action, or even control another creature like a puppet.');
+INSERT INTO schools (school_name, school_description)
+VALUES ('Evocation','Spells manipulate magical energy to produce a desired effect. Some call  up blasts of fire or lightning. Others channel positive energy to heal wounds.');
+INSERT INTO schools (school_name, school_description)
+VALUES ('Illusion','Spells deceive the senses or minds of others. They cause people to see things that are not there, to miss things that are there, to hear phantom noises, or to remember things that never happened. Some illusions create phantom images that any creature can see,  but the most insidious illusions plant an image directly in the mind of a creature.');
+INSERT INTO schools (school_name, school_description)
+VALUES ('Necromancy','Spells manipulate the energies of life and death. Such spells can grant an extra reserve of life force, drain the life energy from another creature, create the undead, or even  bring the dead back to life. Creating the undead through the use of necromancy spells such as animate dead is not a good act, and only evil casters use such spells frequently.');
+INSERT INTO schools (school_name, school_description)
+VALUES ('Transmutation','Spells change the properties of a creature, object, or environment. They might turn an enemy into a harmless creature, bolster the strength of an ally, make an object move at the caster’s command, or enhance a creature’s innate healing abilities to rapidly recover from injury.');
