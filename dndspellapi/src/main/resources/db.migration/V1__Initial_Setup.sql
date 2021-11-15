59590f71d84baca9cf5de102855cd4568bfa79ea
Rose <roset@roses-air.home>
2021-11-15 15:37:10 +0000

    added dnd classes and races info to sql file

--- dndspellapi/src/main/resources/db.migration/V1__Initial_Setup.sql
+++ dndspellapi/src/main/resources/db.migration/V1__Initial_Setup.sql
@@ -69,3 +69,46 @@ INSERT INTO schools (school_name, school_description)
 VALUES ('Necromancy','Spells manipulate the energies of life and death. Such spells can grant an extra reserve of life force, drain the life energy from another creature, create the undead, or even  bring the dead back to life. Creating the undead through the use of necromancy spells such as animate dead is not a good act, and only evil casters use such spells frequently.');
 INSERT INTO schools (school_name, school_description)
 VALUES ('Transmutation','Spells change the properties of a creature, object, or environment. They might turn an enemy into a harmless creature, bolster the strength of an ally, make an object move at the caster’s command, or enhance a creature’s innate healing abilities to rapidly recover from injury.');
+
+INSERT INTO dndclasses (class_name, class_description)
+VALUES ('Bard','An inspiring magician whose power echoes the music of creation.');
+INSERT INTO dndclasses (class_name, class_description)
+VALUES ('Cleric','A priestly champion who wields divine magic in service of a higher power.');
+INSERT INTO dndclasses (class_name, class_description)
+VALUES ('Druid','A priest of the Old  Faith, wielding the powers of nature, moonlight and plant growth, fire and lightning, and adopting animal forms.');
+INSERT INTO dndclasses (class_name, class_description)
+VALUES ('Paladin','A holy warrior bound to a sacred oath.');
+INSERT INTO dndclasses (class_name, class_description)
+VALUES ('Sorcerer','A spellcaster who draws on inherent magic from a gift or bloodline.');
+INSERT INTO dndclasses (class_name, class_description)
+VALUES ('Warlock','A wielder of magic that is derived from a bargain with an extraplanar entity');
+INSERT INTO dndclasses (class_name, class_description)
+VALUES ('Wizard','A scholarly magic-user capable of manipulating the structures of reality.');
+INSERT INTO dndclasses (class_name, class_description)
+VALUES ('Ranger','A warrior who uses  martial prowess and nature magic to combat threats on the edges of civilization.');
+
+INSERT INTO dndraces (race_name, race_description)
+VALUES ('Dwarf','');
+INSERT INTO dndraces (race_name, race_description)
+VALUES ('Halfling','');
+INSERT INTO dndraces (race_name, race_description)
+VALUES ('Half-Elf','');
+INSERT INTO dndraces (race_name, race_description)
+VALUES ('Human','');
+INSERT INTO dndraces (race_name, race_description)
+VALUES ('Elf','');
+INSERT INTO dndraces (race_name, race_description)
+VALUES ('Dragonborn','');
+INSERT INTO dndraces (race_name, race_description)
+VALUES ('Half-Orc','');
+INSERT INTO dndraces (race_name, race_description)
+VALUES ('Tiefling','');
+INSERT INTO dndraces (race_name, race_description)
+VALUES ('Gnome','');
+
+
+
+
+
+
+
